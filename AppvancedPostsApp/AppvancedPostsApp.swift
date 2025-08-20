a0e404dc85ba6cd8a38a3acd462a168f49bbd08b
carla-voneicken <carlavoneicken@googlemail.com>
2025-08-20 21:23:27 +0200

    Upload finished project to GitHub

---
+++ AppvancedPostsApp/AppvancedPostsApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppvancedPostsApp.swift
+//  AppvancedUebung
+//
+//  Created by Carla von Eicken on 17.07.25.
+//
+
+import SwiftUI
+
+@main
+struct AppvancedPostsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            UsersView()
+        }
+    }
+}
